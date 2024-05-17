49daa6ba2a6380fafdd7baae6f905a14c421e8a3
liulianfa <lianfa.liu@icloud.com>
2024-05-17 16:12:21 +0800

    提交测试

--- QtWidgetsApplication1/main.cpp
+++ QtWidgetsApplication1/main.cpp
@@ -8,6 +8,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	cout << "main函数开始执行！" << endl;
+	
+	// 测试提交
+
     QApplication a(argc, argv);
     QtWidgetsApplication1 w;
     w.show();
